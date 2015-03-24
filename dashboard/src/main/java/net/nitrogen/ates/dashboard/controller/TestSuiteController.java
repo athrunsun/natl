d79720a82aaa9ca293379d6ee388aca0cdfd825e
qauser <allenchliu@hotmail.com>
2015-03-24 11:50:37 +0800

    Added details page. TODO: fetch suite cases only.

--- dashboard/src/main/java/net/nitrogen/ates/dashboard/controller/TestSuiteController.java
+++ dashboard/src/main/java/net/nitrogen/ates/dashboard/controller/TestSuiteController.java
@@ -6,6 +6,8 @@ import net.nitrogen.ates.core.enumeration.ExecResult;
 import net.nitrogen.ates.core.model.CustomEnvModel;
 import net.nitrogen.ates.core.model.QueueEntryModel;
 import net.nitrogen.ates.core.model.ExecutionModel;
+import net.nitrogen.ates.core.model.TestCaseModel;
+import net.nitrogen.ates.core.model.TestCaseWithResult;
 import net.nitrogen.ates.core.model.TestSuiteModel;
 import net.nitrogen.ates.util.StringUtil;
 
@@ -22,6 +24,18 @@ public class TestSuiteController extends Controller {
     public void detail() {
         long suiteId = getParaToLong(0);
         setAttr("testsuite", TestSuiteModel.me.findById(suiteId));
+        ControllerHelper.setExecResultEnumAttr(this);
+        setAttr("testCaseWithResultList", getResultList(TestCaseModel.me.findTestCases(ControllerHelper.getProjectPrefFromCookie(this))));
         render("detail.html");
+    }
+
+    private List<TestCaseWithResult> getResultList(List<TestCaseModel> entries) {
+        List<TestCaseWithResult> entriesWithResults = new ArrayList<>();
+
+        for (TestCaseModel entry : entries) {
+            entriesWithResults.add(new TestCaseWithResult(entry));
+        }
+
+        return entriesWithResults;
     }
 }
