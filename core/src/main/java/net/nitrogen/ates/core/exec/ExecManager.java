5a2e3a09f53f7b6b9ffe81c52d9199393f0d7644
qauser <allenchliu@hotmail.com>
2015-04-07 11:27:15 +0800

    Update daemon JVM option for command line with CustomParameterModel

--- core/src/main/java/net/nitrogen/ates/core/exec/ExecManager.java
+++ core/src/main/java/net/nitrogen/ates/core/exec/ExecManager.java
@@ -1,17 +1,20 @@
 package net.nitrogen.ates.core.exec;
 
+import java.io.IOException;
+import java.util.List;
+
+import net.nitrogen.ates.core.enumeration.CustomParameterDomainKey;
+import net.nitrogen.ates.core.enumeration.CustomParameterType;
 import net.nitrogen.ates.core.env.EnvParameter;
+import net.nitrogen.ates.core.model.CustomParameterModel;
 import net.nitrogen.ates.core.model.ProjectModel;
 import net.nitrogen.ates.core.model.QueueEntryModel;
 import net.nitrogen.ates.core.model.SlaveModel;
 import net.nitrogen.ates.util.StringUtil;
+
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.Executor;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ExecManager {
     public static final String EXEC_PARAM_KEY_DEBUG = "nitrogen_ates_debug";
@@ -32,20 +35,10 @@ public class ExecManager {
         QueueEntryModel entry = QueueEntryModel.me.fetchEntry(EnvParameter.machineName());
 
         if (entry != null) {
-            //Map<String, String> envVars = new HashMap<>();
-            String[] rawJvmOptions = entry.getJvmOptions().split(" -D");
-            List<String> jvmOptions = new ArrayList<>();
-
-            for(String rawJvmOption : rawJvmOptions) {
-                rawJvmOption = rawJvmOption.trim();
-                if(!StringUtil.isNullOrWhiteSpace(rawJvmOption)) {
-                    if(rawJvmOption.startsWith("-D")) {
-                        jvmOptions.add(rawJvmOption);
-                    }else {
-                        jvmOptions.add(String.format("-D%s", rawJvmOption));
-                    }
-                }
-            }
+            List<CustomParameterModel> rawJvmOptions = CustomParameterModel.me.findParameters(
+                    CustomParameterDomainKey.EXECUTION,
+                    entry.getExecutionId(),
+                    CustomParameterType.JVM);
 
             CommandLine cmdLine = new CommandLine("java.exe");
             cmdLine.addArgument(String.format("-D%s=%s", EXEC_PARAM_KEY_DEBUG, "0"), false);
@@ -54,13 +47,14 @@ public class ExecManager {
             cmdLine.addArgument(String.format("-D%s=%s", EXEC_PARAM_KEY_EXECUTION_ID, String.valueOf(entry.getExecutionId())), false);
             cmdLine.addArgument(String.format("-D%s=%s", EXEC_PARAM_KEY_PROJECT_ID, String.valueOf(entry.getProjectId())), false);
             cmdLine.addArgument(String.format("-D%s=%s", EXEC_PARAM_KEY_ENV, entry.getEnv()), false);
-            //cmdLine.addArgument(entry.getJvmOptions(), false);
-            for(String jvmOption : jvmOptions) {
-                cmdLine.addArgument(jvmOption, false);
+            for (CustomParameterModel rawJvmOption : rawJvmOptions) {
+                cmdLine.addArgument(String.format("-D%s=%s", rawJvmOption.getKey(), rawJvmOption.getValue()), false);
             }
 
             cmdLine.addArgument("-cp", false);
-            cmdLine.addArgument(String.format("\"%s;%s\"", ProjectModel.me.findJarWithDependencyName(entry.getProjectId()), TEST_RESULT_REPORTER_JAR_PATH), false);
+            cmdLine.addArgument(
+                    String.format("\"%s;%s\"", ProjectModel.me.findJarWithDependencyName(entry.getProjectId()), TEST_RESULT_REPORTER_JAR_PATH),
+                    false);
             cmdLine.addArgument("org.testng.TestNG", false);
             cmdLine.addArgument("-testclass", false);
             cmdLine.addArgument(entry.getName().substring(0, entry.getName().lastIndexOf(".")), false);
@@ -70,16 +64,16 @@ public class ExecManager {
             cmdLine.addArgument(TEST_RESULT_REPORTER_CLASS_NAME, false);
             cmdLine.addArgument("-excludegroups", false);
             cmdLine.addArgument("atesonly", false);
-            cmdLine.addArgument(entry.getParams(), false);
+            // cmdLine.addArgument(entry.getParams(), false); // Function to be Added
             System.out.println("cmdLine.toString:" + cmdLine.toString());
 
             QueueEntryExecResultHandler resultHandler = new QueueEntryExecResultHandler(entry.getId());
 
-            //ExecuteWatchdog watchdog = new ExecuteWatchdog(60 * 1000);
+            // ExecuteWatchdog watchdog = new ExecuteWatchdog(60 * 1000);
             Executor executor = new DefaultExecutor();
             executor.setWorkingDirectory(StringUtil.combinePath("C:/ates/project", String.valueOf(entry.getProjectId())));
-            //executor.setExitValue(1);
-            //executor.setWatchdog(watchdog);
+            // executor.setExitValue(1);
+            // executor.setWatchdog(watchdog);
 
             try {
                 executor.execute(cmdLine, resultHandler);
@@ -94,11 +88,11 @@ public class ExecManager {
             }
 
             // For debug purpose
-//            try {
-//                Thread.sleep(10000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+            // try {
+            // Thread.sleep(10000);
+            // } catch (InterruptedException e) {
+            // e.printStackTrace();
+            // }
         }
     }
 }
